d55763a873a8b217a412f25a2906bae301748846
TrescherDe <TrescherDe@users.noreply.github.com>
2024-12-13 13:20:35 +0000

    updated the logic to use the results of the NN

--- include/check_image_data.hpp
+++ include/check_image_data.hpp
@@ -7,6 +7,7 @@
 #include <cv_bridge/cv_bridge.hpp>
 #include <opencv2/opencv.hpp>
 #include "sensor_msgs/msg/image.hpp"
+#include <sensor_msgs/msg/camera_info.hpp>
 
 // pybind11 for Python-C++ integration
 #include <pybind11/embed.h>  
@@ -28,14 +29,19 @@ public:
 private:
     void imageCallback(const sensor_msgs::msg::Image::SharedPtr msg);
     bool processImage(const sensor_msgs::msg::Image::SharedPtr msg);
+    void publishCameraInfo(const std_msgs::msg::Header& header);
 
     rclcpp::Node::SharedPtr nh_;
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr marked_image_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info_pub_;
 
-    // Add visibility attribute to detect_objects
     pybind11::object detect_objects; // Member visibility matches the class
 
     bool object_detected_ = false;
+    bool nn_ready_ = false;
+    bool m_debug = false;
+
     int compound_coef = 0;
     float threshold = 0.8;
     float nms_threshold = 0.05;
