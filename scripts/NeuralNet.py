af74b705c4f5cae2fdad45cb0072cb2653c7c9a7
TrescherDe <TrescherDe@users.noreply.github.com>
2025-02-05 12:14:49 +0100

    Implemented inference and publishing of detected objects, changed the model to hef

--- scripts/NeuralNet.py
+++ scripts/NeuralNet.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+
 import cv2
 import numpy as np
 from cv_bridge import CvBridge
 
-from multiprocessing import Process
+from vision_msgs.msg import Detection2D, Detection2DArray, ObjectHypothesisWithPose, BoundingBox2D, Point2D, Pose2D
+from sensor_msgs.msg import Image
+from std_msgs.msg import Header
+
 from hailo_platform import (HEF, VDevice, HailoStreamInterface, InferVStreams, ConfigureParams,
 InputVStreamParams, OutputVStreamParams, InputVStreams, OutputVStreams,FormatType)
 
@@ -14,11 +17,10 @@ InputVStreamParams, OutputVStreamParams, InputVStreams, OutputVStreams,FormatTyp
 class NeuralNet(Node):
     def __init__(self):
         super().__init__('neural_net')
-
         self.subscription = self.create_subscription(Image, '/creature_detection/image_raw', self.image_callback, 1)
-        self.image_pub = self.create_publisher(Image, '/creature_detection/marked_image', 1)
-
+        self.bbox_pub_ = self.create_publisher(Detection2DArray, '/creature_detection/bboxes', 1)
         self.bridge = CvBridge()
+        self.m_debug = False
 
     def image_callback(self, msg):
         """ Callback function for processing incoming images """
@@ -41,10 +43,129 @@ class NeuralNet(Node):
         # Apply letterbox padding
         image_padded, scale, pad_w, pad_h = self.letterbox_image(image_rgb)
 
-        # Print letterbox debug info
-        self.get_logger().info(f"Image padded: Scale={scale}, Pad W={pad_w}, Pad H={pad_h}")
+        if self.m_debug:
+            self.get_logger().info(f"Image padded: Scale={scale}, Pad W={pad_w}, Pad H={pad_h}")
 
-        self.get_logger().info("preprocessed the image!")
+        # Add batch dimension
+        if len(image_padded.shape) == 3:
+            image_padded = np.expand_dims(image_padded, axis=0) # Shape (1, 640, 640, 3)
+
+        if self.m_debug:
+            self.get_logger().info(f"Input shape after batch dimension: {image_padded.shape}")
+
+        target = VDevice()
+        hef_path = '/home/autonohm/ros_ws/src/mowing_robot_bt/model/hedgehog_yolov8n.hef'
+        hef = HEF(hef_path)
+
+        # Configure network groups
+        configure_params = ConfigureParams.create_from_hef(hef=hef, interface=HailoStreamInterface.PCIe)
+        network_groups = target.configure(hef, configure_params)
+        network_group = network_groups[0]
+        network_group_params = network_group.create_params()
+
+        # Create input and output virtual streams params
+        input_vstreams_params = InputVStreamParams.make(network_group, format_type=FormatType.FLOAT32)
+        output_vstreams_params = OutputVStreamParams.make(network_group, format_type=FormatType.FLOAT32)        
+
+        input_vstream_info = hef.get_input_vstream_infos()[0]
+        output_vstream_info = hef.get_output_vstream_infos()[0]
+
+        if self.m_debug:
+            self.get_logger().info(f"Expected input shape: {input_vstream_info.shape}")        
+
+        # Infer
+        with InferVStreams(network_group, input_vstreams_params, output_vstreams_params) as infer_pipeline:
+            input_data = {input_vstream_info.name: image_padded}
+            with network_group.activate(network_group_params):
+                infer_results = infer_pipeline.infer(input_data)
+
+        # Process and Display Results
+        output_data = infer_results[output_vstream_info.name]
+        output_data = np.array(output_data)
+
+        if self.m_debug:
+            print("Inference Output Shape:", output_data.shape)
+            print("Inference Results:", output_data)
+
+        self.publish_detections(output_data)
+
+        
+    def publish_detections(self, output_data):
+        if output_data.shape[2] == 0 or output_data.shape[1] == 0:
+            self.get_logger().warn("No detections found. Skipping publish.")
+            return
+
+        detection_array = Detection2DArray()
+        detection_array.header = Header()
+        detection_array.header.stamp = self.get_clock().now().to_msg()
+        detection_array.header.frame_id = "map"     
+
+        # Iterate over detected objects
+        for i in range(output_data.shape[2]):
+            try:
+                ymin, xmin, ymax, xmax, confidence = output_data[0, 0, i, :]
+
+                if confidence < 0.5:
+                    continue
+
+                # Convert normalized values to image scale
+                xmin *= 640
+                ymin *= 640
+                xmax *= 640
+                ymax *= 640
+
+                # Remove the letterbox padding
+                ymin -= 80
+                ymax -= 80
+
+                # Create Detection2D message
+                # bounding box
+                detection = Detection2D()
+                detection.header = Header()
+                detection.header.stamp = self.get_clock().now().to_msg()
+                detection.header.frame_id = "map"
+
+                position_center = Point2D()
+                position_center.x = float((xmin + xmax) / 2.0)
+                position_center.y = float((ymin + ymax) / 2.0)
+
+                bbox_center = Pose2D()
+                bbox_center.position = position_center
+                bbox_center.theta = 0.0
+
+                bbox = BoundingBox2D()
+                bbox.center = bbox_center
+                bbox.size_x = float(xmax - xmin)
+                bbox.size_y = float(ymax - ymin)
+
+                detection.bbox = bbox 
+
+                # classification result
+                hypothesis = ObjectHypothesisWithPose()
+                hypothesis.hypothesis.class_id = "hedgehog" # only one class exists
+                hypothesis.hypothesis.score = float(confidence)
+
+                detection.results.append(hypothesis)
+
+                if self.m_debug:
+                    print("Detection Before Append:")
+                    print(f" - Position_center X: {detection.bbox.center.position.x} ")
+                    print(f" - Position_center Y: {detection.bbox.center.position.y} ")
+                    print(f" - Size X: {detection.bbox.size_x} ({type(detection.bbox.size_x)})")
+                    print(f" - Size Y: {detection.bbox.size_y} ({type(detection.bbox.size_y)})")
+                    print(f" - Results: {detection.results}")
+
+                detection_array.detections.append(detection)
+
+            except IndexError:
+                self.get_logger().error(f"IndexError: output_data[{i}] is out of bounds.")
+                continue
+
+        if detection_array.detections:
+            self.bbox_pub_.publish(detection_array)
+            self.get_logger().info(f"Published {len(detection_array.detections)} detections.")
+        else:
+            self.get_logger().warn("No valid detections after filtering. Skipping publish.")
 
 
     def letterbox_image(self, image, model_w=640, model_h=640):
@@ -69,14 +190,6 @@ class NeuralNet(Node):
         padded_image = cv2.copyMakeBorder(resized, pad_h, pad_h, pad_w, pad_w, cv2.BORDER_CONSTANT, value=(0, 0, 0))
 
         return padded_image, scale, pad_w, pad_h
-
-    def debug_print_values(self, image, title):
-        """ Print sample pixel values for debugging """
-        print(f"\n{title} - Sample Values (Top-left 5x5):")
-        for i in range(min(5, image.shape[0])):
-            for j in range(min(5, image.shape[1])):
-                print(f"{image[i, j]:.2f}", end=" ")
-            print()
 
 def main(args=None):
     rclpy.init(args=args)
